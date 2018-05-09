a5a9225d438225796e01a419e94dc392a160e911
HabibRayan <habibrayan71@gmail.com>
2018-05-09 17:27:39 +0600

    third

--- home-page.php
+++ home-page.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="bootstrap/css/font-awesome.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css">
     <link rel="stylesheet" href="bootstrap/css/font-awesome.min.css">
 
     <script src="bootstrap/js/jquery-3.3.1.min.js"></script>
@@ -14,19 +15,17 @@
     <script src="bootstrap/js/simpleCart.min.js"></script>
 </head>
 <body>
-<br>
-<div class="container">
-<div class="container" style="text-align: center">
-    <div class="row">
-        <a class="" href="">
-            <img src="https://woocrack.com/wp-content/uploads/2016/12/woocrackv3.png" alt="Woocrack" width="240">
-        </a>
-    </div>
-</div>
-<br>
 
-    <div class="row" style="margin-left: 200px">
-        <div class="col-md-4 col-md-offset-3" >
+<div class="container" style="text-align: center">
+  <div class="row">
+      <img src="bootstrap/image/woocrackv3.png" alt="" width="240">
+  </div>
+
+
+</div>
+<div class="container"
+    <div class="row" style="text-align: center">
+        <div class="col-md-4 col-md-offset-3">
             <form action="" class="search-form">
                 <div class="form-group has-feedback">
                     <label for="search" class="sr-only">Search</label>
@@ -36,55 +35,71 @@
             </form>
         </div>
     </div>
-
 </div>
-<br>
+    <div class="container ">
+       <nav class="navbar " id="my-navbar">
+           <div class="" style="background:#324C69;">
+               <div class="navbar-header">
+                   <button style="background:white;" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navber-collapse">
+                      <span class="icon-bar" ></span>
+                       <span class="icon-prev" ></span>
+                       <span class="fa-i-cursor" ></span>
 
-<div class="container">
-    <div class="navbar col-ms-12" style="background:#3B5B7E;">
-        <ul class="nav navbar-nav" >
-            <li >
-                <a href=""><strong style="color: #ffffff"><i class="glyphicon glyphicon-home"></i>Home</strong></a>
+                   </button>
 
-            </li>
-            <li>
-                <a href=""><strong style="color: #ffffff"><i class="glyphicon glyphicon-align-justify"></i> Category</strong></a>
-            </li>
-            <li class="dropdown active">
-                <a href="#" class="dropdown-toggle nav-stylehead" data-toggle="dropdown">
-                    <strong style="color: #ffffff">
-                    <i class="glyphicon glyphicon-refresh fa-spin" aria-hidden="true"></i>
-                      Update<b class="caret"></b></strong>
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a href="">Update History</a> </li>
-                    <li><a href="">How To Update</a> </li>
-                </ul>
-            </li>
-            <li>
-                <a href=""><strong style="color: #ffffff"><i class="glyphicon glyphicon-tag"></i> Shop</strong></a>
-            </li>
-            <li style="background:rgba(0,255,124,0.87);">
+               </div>
+                  <div class="collapse navbar-collapse" id="navber-collapse">
+                      <ul class="nav navbar-nav"  >
+                  <li >
+                      <a href=""><strong style="color: #ffffff"><i class="glyphicon glyphicon-home"></i>Home</strong></a>
+                  </li>
+                  <li>
+                      <a href=""><strong style="color: #ffffff"><i class="glyphicon glyphicon-align-justify"></i> Category</strong></a>
+                  </li>
+                  <li class="dropdown active">
+                      <a href="#" class="dropdown-toggle nav-stylehead" data-toggle="dropdown">
+                          <strong style="color: #ffffff">
+                              <i class="glyphicon glyphicon-refresh fa-spin" aria-hidden="true"></i>
+                              Update<b class="caret"></b></strong>
+                      </a>
+                      <ul class="dropdown-menu">
+                          <li><a href="">Update History</a> </li>
+                          <li><a href="">How To Update</a> </li>
+                      </ul>
+                  </li>
+                  <li>
+                      <a href=""><strong style="color: #ffffff"><i class="glyphicon glyphicon-tag"></i> Shop</strong></a>
+                  </li>
+                  <li style="background:rgba(0,255,124,0.87);">
 
-                <a href=""><strong style="color: #ffffff;"> Join Now</strong></a>
-            </li>
-            <li>
-            <a href=""><strong style="color: #ffffff"><i class=" glyphicon glyphicon-log-in"></i> Login</strong></a>
-            </li>
-        </ul>
-        <div class="" style="">
-            <ul class="nav navbar-nav" style="float:right;margin-right: 10px" >
-                <li style="background: rgba(8,112,250,0.78)">
-                    <a href="">
-                <strong style="color:#ffffff;"><i class="glyphicon glyphicon-shopping-cart"></i> CART</strong>
-                    </a>
+                      <a href=""><strong style="color: #ffffff;"> Join Now</strong></a>
+                  </li>
+                  <li>
+                      <a href=""><strong style="color: #ffffff"><i class=" glyphicon glyphicon-log-in"></i> Login</strong></a>
+                  </li>
+              </ul>
+          <div>
+              <ul class="nav navbar-nav" style="float: right;" >
+                 <li style="background: rgba(8,112,250,0.78);" >
+                  <a href="">
+                      <strong style="color:#ffffff;"><i class="glyphicon glyphicon-shopping-cart"></i> CART</strong>
+                  </a>
                 </li>
-            </ul>
-        </div>
+              </ul>
+          </div>
 
-    </div
-
+      </div>
+  </div>
+</nav>
+    <p align="center" style="background:#324C69;padding:10px;margin-bottom:20px">
+        <b style="color:#B6D8FF;text-transform:capitalize">Join The Club, And Access <u style="color:#fff;">1980</u> Items For Free - </b>
+        <a href="">
+            <button type="button" class="button-join" style="background:#AE4E12;color:#fff;"> <b>JOIN CLUB NOW</b>
+            </button>
+        </a>
+    </p>
 </div>
+
 
 
 </body>
