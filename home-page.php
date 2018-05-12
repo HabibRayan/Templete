87497d343ace1bb55d3eb1022bf0cd897d2e5ec3
HabibRayan <habibrayan71@gmail.com>
2018-05-12 17:14:15 +0600

    six

--- home-page.php
+++ home-page.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css">
     <link rel="stylesheet" href="bootstrap/css/font-awesome.min.css">
     <link rel="stylesheet" href="bootstrap/css/styel.css">
+    <link rel="stylesheet" href="bootstrap/css/autoptimize.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -19,16 +20,18 @@
 </head>
 <body>
 
-<div class="container" style="text-align: center">
-  <div class="row">
-      <img src="bootstrap/image/woocrackv3.png" alt="" width="240">
-  </div>
+
+<div class="container">
+           <div style="text-align: center">
+               <div class="row">
+                  <img src="bootstrap/image/woocrackv3.png" alt="" width="240">
+               </div>
 
 
-</div>
-<div class="container"
-    <div class="row" style="text-align: center">
-        <div class="col-md-4 col-md-offset-3">
+           </div>
+       <div class="container"
+        <div class="row" style="text-align: center">
+          <div class="col-md-6 col-md-offset-3">
             <form action="" class="search-form">
                 <div class="form-group has-feedback">
                     <label for="search" class="sr-only">Search</label>
@@ -36,16 +39,16 @@
                     <span class="glyphicon glyphicon-search form-control-feedback"></span>
                 </div>
             </form>
+          </div>
         </div>
-      </div>
-    <div class="container ">
+    <div class="">
        <nav class="navbar " id="my-navbar">
            <div class="" style="background:#324C69;">
                <div class="navbar-header">
-                   <button style="background:white;" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navber-collapse">
-                      <span class="icon-bar" ></span>
-                      <span class="icon-bar" ></span>
-                      <span class="icon-bar" ></span>
+                   <button style="background:white;" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navber-collapse">
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar "></span>
                    </button>
 
                </div>
@@ -78,68 +81,75 @@
                   <li>
                       <a href=""><strong style="color: #ffffff"><i class=" glyphicon glyphicon-log-in"></i> Login</strong></a>
                   </li>
-              </ul>
-          <div>
-              <ul class="nav navbar-nav" style="float: right;" >
-                 <li style="background: rgba(8,112,250,0.78);" >
-                  <a href="">
-                      <strong style="color:#ffffff;"><i class="glyphicon glyphicon-shopping-cart"></i> CART</strong>
-                  </a>
-                </li>
-              </ul>
-          </div>
+                          <li style="background: rgba(8,112,250,0.78);float: right;" >
+                              <a href="" >
+                                  <strong style="color:#ffffff;"><i class="glyphicon glyphicon-shopping-cart"></i> CART</strong>
+                              </a>
+                          </li>
+                </ul>
+                      <div>
+                      <ul class="nav"  style="float: right;">
 
-      </div>
-  </div>
-</nav>
-    <p align="center" style="background:#324C69;padding:10px;margin-bottom:20px">
-        <b style="color:#B6D8FF;text-transform:capitalize">Join The Club, And Access <u style="color:#fff;">1980</u> Items For Free - </b>
-        <a href="">
+                      </ul>
+                      </div>
+
+              </div>
+           </div>
+      </nav>
+    </div>
+
+           <div>
+             <p align="center" style="background:#324C69;padding:10px;margin-bottom:20px">
+              <b style="color:#B6D8FF;text-transform:capitalize">Join The Club, And Access <u style="color:#fff;">1980</u> Items For Free - </b>
+               <a href="">
             <button type="button" class="button-join" style="background:#AE4E12;color:#fff;"> <b>JOIN CLUB NOW</b>
             </button>
-        </a>
-    </p>
+               </a>
+             </p>
+           </div>
 
 <!--        / !--Home Content */-->
-        <div>
-         <div class="row">
-            <div class="col-xs-6 col-md-3">
-                <a href="#" class="thumbnail">
-                    <img src="bootstrap/image/fff.png" alt="...">
-                </a>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <a href="#" class="thumbnail">
-                    <img src="bootstrap/image/fff.png" alt="...">
-                </a>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <a href="#" class="thumbnail">
-                    <img src="bootstrap/image/fff.png" alt="...">
-                </a>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <a href="#" class="thumbnail">
-                    <img src="bootstrap/image/fff.png" alt="...">
-                </a>
-            </div>
-        </div>
-      </div>
 
 
 
-</div>
+              <!-- Next page/previous -->
+
+            <div style="text-align: center">
+                <ul class="pagination">
+                    <li class="disabled">
+                        <span>Page 1 of 38:</span>
+                    </li>
+                    <li class="active">
+                        <a href="" title="1">1</a>
+                    </li>
+                    <li>
+                        <a href="" title="2">2</a>
+                    </li>
+                    <li class="disabled">
+                        <span>...</span>
+                    </li>
+                    <li>
+                        <a href="" title="Last Page">38</a>
+                    </li>
+                    <li>
+                        <a href="" >Next &raquo;</a>
+                    </li>
+                </ul>
+            </div>
+
+
+
 <!--         footer section-->
-<div class="container">
-<footer >
 
-    <div class="container" >
-            <div class="row" style="text-align: center">
+
+           <footer >
+
+              <div class="row" style="text-align: center">
                 <p><strong style="color: #3B5B7E;">Stay Connected With Me...</strong></p>
                 <a href="" style="color: ;font-size: 50px;" ><i class="fa fa-facebook"></i></a>
                 <a href="" style="color: rgba(22,102,250,0.78);font-size: 50px;padding: 15px" ><i class="fa fa-twitter"></i></a>
                 <a href="" style="color: ;font-size: 50px;" ><i class="fa fa-google-plus"></i></a>
-            </div>
+              </div>
             <div class="row xfoot" style=" ">
                 <div style="text-align: center; color: white;margin-top: 15px">
                     <a href=""><strong style="color: white;padding: 10px"> Terms &amp; conditions</strong></a>
@@ -148,18 +158,13 @@
                     <a href=""><strong style="color: white;padding: 10px">Sitemap</strong></a>
                 </div>
             </div>
-    </div>
 
-    <div class="copyright" style="text-align: center; padding: 10px">
-
-        <div class="container">
+            <div class="copyright" style="text-align: center; padding: 10px">
             <div class="footercredits">Copyright © Woocrack | powered by Pim Studio</div>
-        </div>
-    </div>
-    </div>
-</footer>
-</div>
+            </div>
+          </footer>
 
+</div>
 
 
 </body>
---
+++ test.php
@@ -0,0 +1,50 @@
+<!doctype html>
+
+<html>
+<head>
+    <title>Home</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="bootstrap/css/test.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body style="background-color: #D4DFE4">
+<section>
+    <div class="container-fluid">
+        <div class="row" style="margin-top: 20px">
+            <div class="container">
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" style="padding-right:1px " >
+                    <div>
+                    <a href="https://woocrack.com/product-category/wordpress-plugins/">
+                    <div class="imageBox ">
+                        <img src="bootstrap/image/2.jpg" class="img-responsive">
+                        <div>
+
+                            <h3 class="textPositionH3">Version <br>
+                                2.2.2
+                            </h3>
+
+                            <div style="padding: 5px">
+                                <strong>product Name</strong>
+                            </div>
+                            <div style="padding: 5px">
+                               <a href=""> <button>Download</button></a>
+                               <a href=""><button>Add To Cart</button></a>
+                               <button>$5.00</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    </a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+</section>
+
+
+</body>
+</html>
+
